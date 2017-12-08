0588935585399a31088c2ff6eb49e494f26b3ba1
baiyu <baiyu@admaster.com.cn>
2017-12-08 18:31:17 +0800

    add examples

---
+++ goweb/basic/complex.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c complex64 = 5 + 5i
+	//output: (5+5i)
+	fmt.Printf("Value is: %v", c)
+}
---
+++ thegoprogramminglanguage/ch02/easy_align.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i = 1
+	var s = "ab"
+	fmt.Printf("%d, %s", i, s)
+}
