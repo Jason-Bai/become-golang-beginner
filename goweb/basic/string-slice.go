d6a4164f05ec492a0b1e2be5eb389f73649563d8
baiyu <baiyu@admaster.com.cn>
2018-12-14 10:35:53 +0800

    remove all

--- goweb/basic/string-slice.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	s := "hello"
-	s2 := "c" + s[1:]
-	fmt.Printf("%s\n", s2)
-}
--- thegoprogramminglanguage/ch02/easy_align.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var i = 1
-	var s = "ab"
-	fmt.Printf("%d, %s", i, s)
-}
